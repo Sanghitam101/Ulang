e5d69e9be0b80ce8cca1ce62c17e9caa49cceb84
Sanghitam101 <164131140+Sanghitam101@users.noreply.github.com>
2024-03-20 22:19:45 +0800

    Create Ulang.py

---
+++ Ulang.py
@@ -0,0 +1,11 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)import random
+
+mengetik ('ulang ulang ulang ulamg')
